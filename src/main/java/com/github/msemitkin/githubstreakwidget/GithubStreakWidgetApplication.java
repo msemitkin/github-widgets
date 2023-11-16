ff69b0e7eb9fbbd22799d5793b53d83d538d8268
Mykhailo Semitkin <michael.semitkin@gmail.com>
2023-11-16 15:06:44 +0200

    init

---
+++ src/main/java/com/github/msemitkin/githubstreakwidget/GithubStreakWidgetApplication.java
@@ -0,0 +1,13 @@
+package com.github.msemitkin.githubstreakwidget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubStreakWidgetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubStreakWidgetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/msemitkin/githubstreakwidget/GithubStreakWidgetApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.msemitkin.githubstreakwidget;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubStreakWidgetApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertDoesNotThrow(() -> {
+        });
+    }
+
+}
