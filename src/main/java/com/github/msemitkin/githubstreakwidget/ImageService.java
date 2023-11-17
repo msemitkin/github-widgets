c9c141342336b2c1cb614690b1509def8607b79c
Mykhailo Semitkin <michael.semitkin@gmail.com>
2023-11-17 11:22:55 +0200

    encapsulated calculation of image size

--- src/main/java/com/github/msemitkin/githubstreakwidget/ImageService.java
+++ src/main/java/com/github/msemitkin/githubstreakwidget/ImageService.java
@@ -13,17 +13,11 @@ import java.io.UncheckedIOException;
 public class ImageService {
 
     public byte[] createImage(String text) {
-        BufferedImage img = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2d = img.createGraphics();
         Font font = new Font("Arial", Font.PLAIN, 48);
-        g2d.setFont(font);
-        FontMetrics fm = g2d.getFontMetrics();
-        int width = fm.stringWidth(text);
-        int height = fm.getHeight();
-        g2d.dispose();
+        Size imageSize = getImageSize(text, font);
 
-        img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        g2d = img.createGraphics();
+        BufferedImage img = new BufferedImage(imageSize.width(), imageSize.height(), BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = img.createGraphics();
         g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
@@ -33,7 +27,7 @@ public class ImageService {
         g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
         g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
         g2d.setFont(font);
-        fm = g2d.getFontMetrics();
+        FontMetrics fm = g2d.getFontMetrics();
         g2d.setColor(Color.BLACK);
         g2d.drawString(text, 0, fm.getAscent());
         g2d.setBackground(Color.GREEN);
@@ -44,5 +38,19 @@ public class ImageService {
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
+    }
+
+    private Size getImageSize(String text, Font font) {
+        BufferedImage img = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = img.createGraphics();
+        g2d.setFont(font);
+        FontMetrics fm = g2d.getFontMetrics();
+        int width = fm.stringWidth(text);
+        int height = fm.getHeight();
+        g2d.dispose();
+        return new Size(width, height);
+    }
+
+    private record Size(int width, int height) {
     }
 }
